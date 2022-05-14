17130fcb8ef63e9c0ad0e20b6ad145b1a2a9aada
eheperson <barannkuzu@gmail.com>
2022-05-14 04:35:22 +0300

    example cpp mpudle

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
-  
+#include "adder.h"
+float add(float a, float b);
+
 int main(){
 
+    std::cout << "Hey, Zeus!\n";
+
+    std::cout << add(34.5f, 34346.6f) << std :: endl;
     return 0;
 }
